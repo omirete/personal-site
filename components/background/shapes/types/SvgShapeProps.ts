822c8ec2501ce9ec3ff680b8bd0015c61e122056
Olga Mirete <olgamirete@gmail.com>
2023-03-25 14:57:55 +0100

    removed old unused components

--- components/background/shapes/types/SvgShapeProps.ts
+++
@@ -1,7 +0,0 @@
-import Point2D from "@/types/Point2D";
-import { SVGProps } from "react";
-
-export default interface SVGShapeProps<T> extends SVGProps<T> {
-    position?: Point2D;
-    originCoord?: Point2D;
-}
