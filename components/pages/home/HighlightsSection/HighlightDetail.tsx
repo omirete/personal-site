a3b17bcfb6d9788d6a0708b115dc63c28cd5b960
Olga Mirete <olgamirete@gmail.com>
2023-04-01 22:25:55 +0200

    deleted unneeded imports

--- components/pages/home/HighlightsSection/HighlightDetail.tsx
+++ components/pages/home/HighlightsSection/HighlightDetail.tsx
@@ -1,7 +1,7 @@
 import { Highlight } from "@/helpers/database/HighlightsCtor";
 import parseStringI18N from "@/i18n/helpers/parseStringI18N";
 import FCi18n from "@/i18n/types/FCi18n";
-import { DetailedHTMLProps, HTMLAttributes, useEffect, useState } from "react";
+import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 export interface HighlightDetailProps
     extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
